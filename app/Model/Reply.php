f7de3e0ec23229362877e09ec69b39a1bdb9f639
shahid minhas <shahidminhas313@gmail.com>
2021-03-17 12:25:12 +0500

    relastionships

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -2,9 +2,24 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    protected $fillable = ['body','question_id','user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function like(){
+        return $this->hasMany(Like::class);
+    }
 }
