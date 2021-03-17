2683b3a7d330a26d3729cc33fb213736d7baaa02
shahid minhas <shahidminhas313@gmail.com>
2021-03-17 13:18:26 +0500

    make factries and seeding

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,11 +2,21 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Category;
 use App\Model\Question;
 use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title'=>$title,
+        'slug'=>$faker->slug,
+        'body'=> $faker->text,
+        'category_id'=>function(){
+        return Category::all()->random();
+        },
+        'user_id'=>function(){
+        return \App\User::all()->random();
+        }
     ];
 });
