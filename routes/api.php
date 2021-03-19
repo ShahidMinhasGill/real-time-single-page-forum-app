d0b0b8f8b6cb35050c9a81ffaaad4f8d4f93eb8a
Shahid Minhas <shahidminhas313@gmail.com>
2021-03-19 04:44:36 -0700

    category crud

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('/question','QuestionController');
+Route::resource('/category','CategoryController');
