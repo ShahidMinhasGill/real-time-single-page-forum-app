ad7343d75fadea531524d0f1081a54388b0b578e
Shahid Minhas <shahidminhas313@gmail.com>
2021-03-24 00:58:38 -0700

    helpers class and function

---
+++ resources/js/Helpers/User.js
@@ -0,0 +1,55 @@
+import token from "./token";
+import AppStorage from "./AppStorage";
+
+class User {
+    login(data){
+        axios.post('/api/auth/login',data)
+            .then(res=> this.responseAfterLogin(res))
+            .catch(err => console.log(err.response.data))
+    }
+
+    responseAfterLogin(res) {
+      const access_token = res.data.access_token;
+      const username = res.data.user;
+
+        if(token.isValid(access_token)){
+          AppStorage.store(access_token,username)
+        }
+    }
+
+    hasToken() {
+        const storedToken =  AppStorage.getToken()
+
+        if (storedToken){
+           return token.isValid(storedToken) ? true : false
+        }
+        return false
+
+    }
+
+    loggedIn() {
+        return this.hasToken()
+    }
+
+    logout() {
+        AppStorage.clear()
+    }
+
+    name(){
+        return AppStorage.getUser()
+    }
+
+    id(){
+        if(this.loggedIn()){
+            const payload = token.payload(AppStorage.getToken())
+            return payload.sub
+        }
+    }
+
+
+
+
+
+}
+
+export default User = new User();
